76fabfc07aa390c93653ed7b5cea4ada5240a030
Dario Maiocchi <dmaiocchi@suse.com>
2017-10-30 12:29:07 +0100

    fix

--- lib/tango.ex
+++ lib/tango.ex
@@ -12,7 +12,33 @@ defmodule Tango do
       :world
 
   """
-  def hello do
-    :world
+  def init_table_tasks() do
+    :ets.new(:gitarro_tasks, [:set, :protected])
+  end
+
+ # refactor and store values to table
+  def load_yaml(filename) do
+    table_task = init_table_tasks
+    data = :yamerl_constr.file(filename)
+    flat = List.flatten(data)
+    Enum.each flat, fn post -> 
+      case post do
+       {'context', value} -> IO.puts value
+       {'repository', value} -> IO.puts value
+       {'test', value} -> IO.puts value
+      end 
+    end
+#    :ets.insert(table_task, data)
+  end
+end
+
+defmodule Tango.CLI do
+  @moduledoc """
+  Documentation for tango
+  """
+  import Tango
+  def main(args \\ []) do
+    IO.puts('LOADING YML')
+    load_yaml('gitarro_jobs.yml')
   end
 end
--- mix.exs
+++ mix.exs
@@ -7,22 +7,23 @@ defmodule Tango.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      escript: [main_module: Tango.CLI],
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :yamerl]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+       {:credo, "~> 0.8", only: [:dev, :test], runtime: false},
+       {:yamerl, "~> 0.4.0"}
     ]
   end
 end
