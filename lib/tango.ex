b073717091db2a10625e028f8e34508460e018d6
Dario Maiocchi <dmaiocchi@suse.com>
2017-10-30 15:05:48 +0100

    refactoring

--- lib/tango.ex
+++ lib/tango.ex
@@ -3,32 +3,27 @@ defmodule Tango do
   Documentation for Tango.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Tango.hello
-      :world
-
-  """
-  def init_table_tasks() do
+  defp init_table_tasks() do
     :ets.new(:gitarro_tasks, [:set, :protected])
   end
 
- # refactor and store values to table
-  def load_yaml(filename) do
-    table_task = init_table_tasks
-    data = :yamerl_constr.file(filename)
-    flat = List.flatten(data)
-    Enum.each flat, fn post -> 
+  defp scan_yaml(data_yaml) do
+    Enum.each data_yaml, fn post -> 
       case post do
        {'context', value} -> IO.puts value
        {'repository', value} -> IO.puts value
        {'test', value} -> IO.puts value
       end 
     end
-#    :ets.insert(table_task, data)
+  end
+
+  def load_yaml(filename) do
+    table_task = init_table_tasks
+    data = :yamerl_constr.file(filename)
+    flat = List.flatten(data)
+    scan_yaml(flat)
+    # FIXME: insert values on table.
+    #:ets.insert(table_task, data)
   end
 end
 
